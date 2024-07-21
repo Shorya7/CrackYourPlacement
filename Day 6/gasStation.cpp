1c49d4cdda725d02f6d72f58785e8788a6459060
Shorya <shorya2113081@akgec.ac.in>
2024-07-22 02:03:01 +0530

    easy

--- Day 6/gasStation.cpp
+++ Day 6/gasStation.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int sg=0,sc=0,ans,c=0;
+        int sg=0,sc=0,ans=0,c=0;
         for(int i=0;i<gas.size();i++){
             sg+=gas[i];
             sc+=cost[i];
         }
         if(sc>sg)return -1;
         for(int i=0;i<gas.size();i++){
-            c=gas[i]-cost[i];
+            c+=gas[i]-cost[i];
             if(c<0){
                 c=0;
                 ans=i+1;
