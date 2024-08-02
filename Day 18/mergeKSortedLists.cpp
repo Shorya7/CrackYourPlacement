6e1391d80897a43dd2973853170a42d0f8c83573
Shorya <shorya2113081@akgec.ac.in>
2024-08-03 04:23:25 +0530

    hard

--- Day 18/mergeKSortedLists.cpp
+++ Day 18/mergeKSortedLists.cpp
@@ -40,4 +40,35 @@ public:
 };
 
 //------------------------------------------------------------------------------------------------------------------
-// SOLUTION 2
+// SOLUTION 2 (Min Heap)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size()==0)return NULL;
+        ListNode* n=new ListNode(0);
+        ListNode* t=n;
+        priority_queue<pair<int,ListNode*>,vector<pair<int,ListNode*>>,greater<pair<int,ListNode*>>>pq;
+        for(auto i:lists){
+            if(i)pq.push({i->val,i});
+        }
+        while(!pq.empty()){
+            ListNode* m=pq.top().second;
+            pq.pop();
+            if(m->next)pq.push({m->next->val,m->next});
+            t->next=m;
+            t=m;
+        }
+        return n->next;
+    }
+};
