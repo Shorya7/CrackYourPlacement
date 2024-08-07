eabcf6cf7cc9d4390c6f7c9fb9dcc1eabc9ed9ce
Shorya <shorya2113081@akgec.ac.in>
2024-08-08 04:50:11 +0530

    easy

---
+++ Day 24/implementTwoStacksInAnArray.cpp
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class twoStacks {
+  public:
+  int s[100];
+  int t1=-1,t2=100;
+    twoStacks() {}
+
+    // Function to push an integer into the stack1.
+    void push1(int x) {
+        s[++t1]=x;
+    }
+
+    // Function to push an integer into the stack2.
+    void push2(int x) {
+        s[--t2]=x;
+    }
+
+    // Function to remove an element from top of the stack1.
+    int pop1() {
+        if(t1==-1)return -1;
+        else return s[t1--];
+    }
+
+    // Function to remove an element from top of the stack2.
+    int pop2() {
+        if(t2==100)return -1;
+        else return s[t2++];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int T;
+    cin >> T;
+    while (T--) {
+        twoStacks *sq = new twoStacks();
+
+        int Q;
+        cin >> Q;
+        while (Q--) {
+            int stack_no;
+            cin >> stack_no;
+            int QueryType = 0;
+            cin >> QueryType;
+
+            if (QueryType == 1) {
+                int a;
+                cin >> a;
+                if (stack_no == 1)
+                    sq->push1(a);
+                else if (stack_no == 2)
+                    sq->push2(a);
+            } else if (QueryType == 2) {
+                if (stack_no == 1)
+                    cout << sq->pop1() << " ";
+                else if (stack_no == 2)
+                    cout << sq->pop2() << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+// } Driver Code Ends
