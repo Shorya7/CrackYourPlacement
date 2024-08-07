e003a3bfea97b74ec17a1229472a2d3bf26bece3
Shorya <shorya2113081@akgec.ac.in>
2024-08-08 05:27:49 +0530

    medium

--- Day 24/removeAllAdjacentDuplicatesInString-II.cpp
+++ Day 24/removeAllAdjacentDuplicatesInString-II.cpp
@@ -1,3 +1,5 @@
+// SOLUTION 1
+
 class Solution {
 public:
     string removeDuplicates(string s, int k) {
@@ -12,6 +14,32 @@ public:
         string ans;
         for(auto i:v)
         ans.append(i.second,i.first);
+        return ans;
+    }
+};
+
+//----------------------------------------------------------------------------------------------------------------------------
+// SOLUTION 2 (Using STACK)
+
+class Solution {
+public:
+    string removeDuplicates(string str, int k) {
+        stack<pair<char,int>>s;
+        for(char c:str){
+            if(!s.empty()&&s.top().first==c){
+                auto i=s.top();
+                s.pop();
+                if(i.second<k-1)s.push({i.first,i.second+1});
+            }
+            else s.push({c,1});
+        }
+        string ans;
+        while(!s.empty()){
+            while(s.top().second--)
+            ans+=s.top().first;
+            s.pop();
+        }
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
