24708f8d9a9ef2ac7f0f21d82d3bf0a26f19766f
Shorya <shorya2113081@akgec.ac.in>
2024-08-08 03:24:49 +0530

    medium

---
+++ Day 23/largestSubarrayWithZeroSum.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+/*You are required to complete this function*/
+
+class Solution {
+  public:
+    int maxLen(vector<int>& arr, int n) {
+        // Your code here
+        unordered_map<int,int>m;
+        int s=0,ans=0;
+        for(int i=0;i<n;i++){
+            s+=arr[i];
+            if(s==0)ans=i+1;
+            else{
+                if(m.find(s)!=m.end())
+                ans=max(ans,i-m[s]);
+                else m[s]=i;
+            }
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int m;
+        cin >> m;
+        vector<int> array1(m);
+        for (int i = 0; i < m; ++i) {
+            cin >> array1[i];
+        }
+        Solution ob;
+        cout << ob.maxLen(array1, m) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
