b96c3d8586281cf8a82600dd83360f67fe54c66c
Shorya <shorya2113081@akgec.ac.in>
2024-08-10 03:04:59 +0530

    medium

--- Day 25/basicCalculator-II.cpp
+++ Day 25/basicCalculator-II.cpp
@@ -1,3 +1,5 @@
+// SOLUTION 1
+
 class Solution {
 public:
     int calculate(string s) {
@@ -20,6 +22,46 @@ public:
                 op=c;
                 cur=0;
             }
+        }
+        return ans;
+    }
+};
+
+
+//---------------------------------------------------------------------------------------------------------------------------
+//  Solution 2 (Using STACK)
+
+class Solution {
+public:
+    int calculate(string s) {
+        s+='+';
+        stack<int>st;
+        char op='+';
+        int ans=0,cur=0;
+        for(char c:s){
+            if(c==' ')continue;
+            if(isdigit(c))
+            cur=cur*10+(c-'0');
+            else{
+                if(op=='+')st.push(cur);
+                else if(op=='-')st.push(-cur);
+                else if(op=='*'){
+                    int n=st.top();
+                    st.pop();
+                    st.push(n*cur);
+                }
+                else{
+                    int n=st.top();
+                    st.pop();
+                    st.push(n/cur);
+                }
+                cur=0;
+                op=c;
+            }
+        }
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
         }
         return ans;
     }
