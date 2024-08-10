ffda42fe3e5e8acd0b5b24f5010b772180a83269
Shorya <shorya2113081@akgec.ac.in>
2024-08-10 05:30:23 +0530

    medium

--- Day 26/copyListWithRandomPointer.cpp
+++ Day 26/copyListWithRandomPointer.cpp
@@ -1,3 +1,5 @@
+// SOLUTION 1 (Using Hash Map)
+
 /*
 // Definition for a Node.
 class Node {
@@ -32,5 +34,57 @@ public:
             t=t->next;
         }
         return m[head];
+    }
+};
+
+
+//---------------------------------------------------------------------------------------------------------------------------
+
+// SOLUTION 2 (Optimized Approach)
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        Node* t=head;
+        while(t){
+            Node* c=new Node(t->val);
+            c->next=t->next;
+            t->next=c;
+            t=t->next->next;
+        }
+        t=head;
+        while(t){
+            Node* c=t->next;
+            if(t->random)
+            c->random=t->random->next;
+            else c->random=NULL;
+            t=t->next->next;
+        }
+        Node* ans=new Node(0);
+        Node* n=ans;
+        t=head;
+        while(t){
+            n->next=t->next;
+            t->next=t->next->next;
+            n=n->next;
+            t=t->next;
+        }
+        return ans->next;
     }
 };
